8c7e52cb0cb9c64ff42fa8c56df81bcc98f04b11
Emiliano Gandini Outeda <emiliano.gandini@protonmail.com>
2026-06-12 01:58:31 -0300

    added seeds

--- app/models/repo.py
+++ app/models/repo.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import String, Boolean
 from dbwarden.schema import auto_schema
+from dbwarden.seed import Seed
 from dbwarden import CHTableMeta, ChEngineSpec
 
 from app.core.databases import Base
@@ -19,3 +20,23 @@ class Repo(Base):
         ch_order_by = ["name"]
         ch_primary_key = "name"
         comment = "Repo store"
+
+class RepoSeed(Seed):
+    __seed_database__ = "clickhouse"
+    __seed_description__ = "Tracked Repos"
+    __seed_on_conflict__ = "update"
+
+    model = Repo
+    rows = [
+        Repo(name="dbwarden", owner="emiliano-gandini-outeda", is_org=False, default_branch="main"),
+        Repo(name="schemap", owner="emiliano-gandini-outeda", is_org=False, default_branch="master"),
+        Repo(name="crxml", owner="emiliano-gandini-outeda", is_org=False, default_branch="master"),
+        Repo(name="detrack", owner="emiliano-gandini-outeda", is_org=False, default_branch="master"),
+        Repo(name="seoslug", owner="emiliano-gandini-outeda", is_org=False, default_branch="master"),
+        Repo(name="acrresolv", owner="emiliano-gandini-outeda", is_org=False, default_branch="master"),
+        Repo(name="emiliano-gandini-outeda", owner="emiliano-gandini-outeda", is_org=False, default_branch="main"),
+        Repo(name="equinox", owner="emiliano-gandini-outeda", is_org=False, default_branch="main"),
+        Repo(name="ArgosUY", owner="LibreCourseUY", is_org=True, default_branch="main"),
+        Repo(name="UruAPI", owner="LibreCourseUY", is_org=True, default_branch="main"),
+        Repo(name="web", owner="LibreCourseUY", is_org=True, default_branch="main"),
+    ]
