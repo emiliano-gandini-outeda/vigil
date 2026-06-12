df3af345c79bd3dd2cf65cd75bf215f8626cedd1
Emiliano Gandini Outeda <emiliano.gandini@protonmail.com>
2026-06-12 01:58:26 -0300

    Fixed migrations

--- migrations/clickhouse/clickhouse__0001_offline_migration.sql
+++ migrations/clickhouse/clickhouse__0001_offline_migration.sql
@@ -1,10 +1,23 @@
 -- upgrade
 
-CREATE TABLE IF NOT EXISTS repo (
-    name String NOT NULL,
-    owner String NOT NULL,
+CREATE TABLE IF NOT EXISTS commits (
+    author_email String NOT NULL,
+    author_login String NOT NULL,
+    author_name String NOT NULL,
+    committed_at DateTime64(3) NOT NULL,
+    message String,
+    repo NULL NOT NULL REFERENCES repos(name),
+    sha String NOT NULL
+)
+ENGINE = MergeTree()
+ORDER BY (repo, committed_at)
+PRIMARY KEY repo
+
+CREATE TABLE IF NOT EXISTS repos (
+    default_branch String NOT NULL,
     is_org Bool NOT NULL,
-    default_branch String NOT NULL
+    name String NOT NULL,
+    owner String NOT NULL
 )
 ENGINE = ReplacingMergeTree()
 ORDER BY (name)
@@ -12,4 +25,6 @@ PRIMARY KEY name
 
 -- rollback
 
-DROP TABLE repo
+DROP TABLE commits
+
+DROP TABLE repos
