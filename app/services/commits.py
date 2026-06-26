6df8bb68e6334d31cb0a40589b3fa1ea03358cf2
Emiliano Gandini Outeda <emiliano.gandini@protonmail.com>
2026-06-26 00:42:53 -0300

    feat: commit helper function

---
+++ app/services/commits.py
@@ -0,0 +1,11 @@
+
+def extract_commit_data(commit) -> dict:
+    return {
+        "sha": commit.sha,
+        "author_name": commit.commit.author.name,
+        "author_email": commit.commit.author.email,
+        "author_login": commit.author.login if commit.author else None,
+        "committed_at": commit.commit.author.date,
+        "message": commit.commit.message,
+        "is_merge": len(commit.parents) > 1,
+    }
